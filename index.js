a74bbe2bee33dc8f384bb33d28aa22c5edd8738c
Tom Rogers <tomrogers2823@gmail.com>
2018-03-03 02:49:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { share, recover, bigPrime } = require('./lib/index')
 function ShamirSecretSharing (partsToSplitInto, minimumPartsToRecover, prime) {
   this.prime = prime || bigPrime()
   this.share = share(this.prime, partsToSplitInto, minimumPartsToRecover)
-  this.reconstruct = recover(this.prime)
+  this.recover = recover(this.prime)
 }
 
 module.exports = ShamirSecretSharing
